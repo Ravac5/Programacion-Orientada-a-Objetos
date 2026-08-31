ef3636bc13cebce2d0404d9c98776629343eb6f2
Ravac5 <iamravac5@hotmail.com>
2026-08-31 16:35:57 -0300

    ejercicio10 added

---
+++ unidad2/ejercitacion2.1/ejercicio10/ejercicio10main.java
@@ -0,0 +1,20 @@
+public class ejercicio10main {
+    public static void main(String[] args){
+
+        personaje kazuma = new personaje("Kazuma", 70);
+        personaje megumin = new personaje("Megumin", 70);
+
+        while (megumin.estaVivo() == true && kazuma.estaVivo() == true) {
+            kazuma.atacar(megumin);
+            System.out.println(megumin.getNombre() + " tiene: " + megumin.getVida() + " puntos de vida");
+            megumin.atacar(kazuma);
+            System.out.println(kazuma.getNombre() + " tiene: " + kazuma.getVida() + " puntos de vida");
+        }
+
+        if (kazuma.estaVivo() == true){
+            System.out.println(kazuma.getNombre() + " ha ganado!");
+        } else {
+            System.out.println(megumin.getNombre() + " ha ganado!");
+        }
+    }
+}
---
+++ unidad2/ejercitacion2.1/ejercicio10/personaje.java
@@ -0,0 +1,36 @@
+public class personaje{
+    private int puntosVida, puntosAtaque, randomAtaque;
+    private String nombre;
+
+    public personaje(String nombre, int puntosAtaque){
+        this.puntosVida = 100;
+        this.nombre = nombre;
+        this.puntosAtaque = puntosAtaque;
+    }
+
+    public int getVida(){
+        return puntosVida;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void atacar(personaje objetivo){
+        if (objetivo.puntosVida >= this.puntosAtaque){
+            this.randomAtaque = (int)(Math.random() * this.puntosAtaque);
+            objetivo.puntosVida = objetivo.puntosVida - this.randomAtaque;
+        } else {
+            objetivo.puntosVida = 0;
+        }
+        System.out.println(this.nombre + " ha atacado a: " + objetivo.nombre + " Ataque: " + this.randomAtaque);
+    }
+
+    public boolean estaVivo(){
+        if (puntosVida > 0){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
